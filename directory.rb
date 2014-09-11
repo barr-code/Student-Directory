15a34a0a036578c168e0aaa2609a307966b415c5
Victoria <vb329@georgetown.edu>
2014-09-11 20:29:31 +0100

    Created array of hashes for students to make code more readable

--- directory.rb
+++ directory.rb
@@ -1,16 +1,29 @@
 student_count = 15
 
-students = ["Victoria Barr", "Karin Nielsen", "Andrew Snead", "Nicole Tredway", "Rachel Nolan",
-"Tim Scully", "Craig Horsborough", "Camilla van Klinken", "Andrew Hercules", "Peter Saxton",
-"Dario Daprile", "Anna Schechter", "Denise Yu", "Alex Fakhri", "Bernard Mordan"]
+students = [
+{:name => "Victoria Barr", :cohort => :september},
+{:name => "Karin Nielsen", :cohort => :september}, 
+{:name => "Andrew Snead", :cohort => :september}, 
+{:name => "Nicole Tredway", :cohort => :september}, 
+{:name => "Rachel Nolan", :cohort=> :september},
+{:name => "Tim Scully", :cohort => :september}, 
+{:name => "Craig Horsborough", :cohort => :september}, 
+{:name => "Camilla van Klinken", :cohort => :september}, 
+{:name => "Andrew Hercules", :cohort => :september}, 
+{:name => "Peter Saxton", :cohort => :september},
+{:name => "Dario Daprile", :cohort => :september}, 
+{:name => "Anna Schechter", :cohort => :september}, 
+{:name => "Denise Yu", :cohort => :september}, 
+{:name => "Alex Fakhri", :cohort => :september}, 
+{:name => "Bernard Mordan", :cohort => :september}]
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "--------------------"
 end
 
-def print(names)
-	names.each {|name| puts name}
+def print(students)
+	students.each {|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end
 
 def print_footer(names)
