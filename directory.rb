c7a4b423e83a2a02eeca0864bef982e8cb7c5e73
Victoria <vb329@georgetown.edu>
2014-09-12 01:08:03 +0100

    Refactored code, created new methods

--- directory.rb
+++ directory.rb
@@ -1,53 +1,59 @@
+@students = []
+
 def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "--------------------"
 end
 
-def print(students)
-	students.each.with_index(1) {|student, n| puts "#{n}: #{student[:name]} (#{student[:cohort]} cohort)"}
+def print_students_list
+	@students.each.with_index(1) {|student, n| puts "#{n}: #{student[:name]} (#{student[:cohort]} cohort)"}
 end
 
-def print_footer(students)
-	puts "Overall, we have #{students.length} great students."
+def print_footer
+	puts "Overall, we have #{@students.length} great students."
 end
 
 def input_students
 	puts "Please enter the names of the students."
 	puts "To finish, just hit return twice."
 	
-	students = []
-	name = gets.chomp
+		name = gets.chomp
 	while !name.empty? do
-		students << {:name => name, :cohort => :september}
-		puts "Now we have #{students.length} students!"
+		@students << {:name => name, :cohort => :september}
+		puts "Now we have #{@students.length} students!"
 		name = gets.chomp
 	end
-	students
+end
+
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+end
+
+def show_students
+	print_header
+	print_students_list
+	print_footer
+end
+
+def process(selection)
+	case selection
+		when "1"
+			input_students
+		when "2"
+			show_students
+		when "9"
+			exit
+		else
+			puts "I don't know what you meant, try again."
+	end
 end
 
 def interactive_menu
-	students = []
 	loop do
-		puts "Welcome to the Makers Academy Student Directory."
-		puts "What would you like to do?"
-		puts "1. Input the student"
-		puts "2. Show the students"
-		puts "9. Exit"
-		
-		selection = gets.chomp
-		
-		case selection
-			when "1"
-				students = input_students
-			when "2"
-				print_header
-				print(students)
-				print_footer(students)
-			when "9"
-				exit
-			else
-				puts "I don't know what you meant, try again."
-		end
+		print_menu
+		process(gets.chomp)
 	end
 end
 
